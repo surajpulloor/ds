03f562cb432805c29e961c5bd93f345da9d9dbeb
Suraj Pulloor <thegamer600@gmail.com>
2020-05-21 19:08:06 +0530

    Added case statement to handle multiple build targets

--- src/watch-trigger.sh
+++ src/watch-trigger.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 clear
-make
-./main
+
+echo "Targets:"
+echo "ll"
+read -p "Which build?" build
+case $build in
+    ll)
+        make ll
+        ;;
+    *)
+        echo "Wrong input"
+        ;;
+esac
