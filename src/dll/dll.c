713ad653a3eb05bbde0e8058b386941a94bd6547
Suraj Pulloor <thegamer600@gmail.com>
2020-06-01 22:50:23 +0530

    testing code for double linked list

--- src/dll/dll.c
+++ src/dll/dll.c
@@ -1,4 +1,4 @@
-#include "../include/double_linked_list.h"
+#include "../../include/double_linked_list.h"
 #include <stdio.h>
 
 int main()
@@ -18,15 +18,19 @@ int main()
     print_list(list);
 
     char buffer[MAX_BUFFER];
-    push_front(list, "Kick this shit");
-    push_front(list, "The quick brown");
-    push_front(list, "fox jumped over the lazy dog.");
+    // push_before(list, "Kick this shit", 1);
+    // push_before(list, "The quick brown", 1);
+    // push_before(list, "fox jumped over the lazy dog.", 1);
 
-    print_list(list);
+    printf("Pop front\n");
+    printf("Value = %s\n", pop_after_v(list, 1, buffer));
+    printf("Value = %s\n", pop_after_v(list, 3, buffer));
 
-    for (int i = 1; i < 10; i++) {
-        set(list, i, "This will repeat 9 times.");
-    }
+    // print_list(list);
+
+    // for (int i = 1; i < 10; i++) {
+    //     set(list, i, "This will repeat 9 times.");
+    // }
 
     print_list(list);
 
