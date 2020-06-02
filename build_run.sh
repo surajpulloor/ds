c4b0778faa62cfc1f19876c5883c77537f84d5cb
Suraj Pulloor <thegamer600@gmail.com>
2020-06-02 16:28:35 +0530

    Added build command for Stack(Array) ds

--- build_run.sh
+++ build_run.sh
@@ -7,6 +7,7 @@ echo "1) ll"
 echo "2) dll"
 echo "3) cdll"
 echo "4) cll"
+echo "5) stack_array"
 read -p "Which build?" build
 
 case $build in
@@ -30,6 +31,12 @@ case $build in
         make circular_ll
         clear
         ./circular_ll
+        ;;
+
+    5)
+        make stack_array
+        clear
+        ./bin/stack_array
         ;;
     *)
         echo "Wrong input"
