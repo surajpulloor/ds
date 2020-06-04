9df5ace1b7d9e82534fc81cd77203bd9cd53153e
Suraj Pulloor <thegamer600@gmail.com>
2020-06-05 00:50:15 +0530

    Segregated build recipes related to stack array to its own Makefiles

---
+++ src/stack_array/stack_array.mk
@@ -0,0 +1,37 @@
+ROOT_DIR=../..
+
+include $(ROOT_DIR)/Makefile.variable
+
+IDIR:=$(ROOT_DIR)/$(IDIR)
+ODIR:=$(ROOT_DIR)/$(ODIR)
+BIN_DIR:=$(ROOT_DIR)/$(BIN_DIR)
+LIB_DIR:=$(ROOT_DIR)/$(LIB_DIR)
+
+LIB_NAME=stack_array
+
+ODIR_STACK_ARRAY=stack_array
+
+_DEPS_STACK_ARRAY = stack_array.h
+DEPS_STACK_ARRAY = $(patsubst %,$(IDIR)/%,$(_DEPS_STACK_ARRAY))
+
+# Stack_Array objects recipes
+_OBJ_STACK_ARRAY = push_pop.o misc.o
+OBJ_STACK_ARRAY = $(patsubst %,$(ODIR)/$(ODIR_STACK_ARRAY)/%,$(_OBJ_STACK_ARRAY))
+
+_OBJ_MAIN_STACK_ARRAY = main.o
+OBJ_MAIN_STACK_ARRAY = $(patsubst %,$(ODIR)/$(ODIR_STACK_ARRAY)/%,$(_OBJ_MAIN_STACK_ARRAY))
+
+_LIB_NAME = lib$(LIB_NAME).a
+GEN_LIB_STACK_ARRAY = $(patsubst %,$(LIB_DIR)/%,$(_LIB_NAME))
+
+
+$(ODIR)/$(ODIR_STACK_ARRAY)/%.o: %.c $(DEPS_STACK_ARRAY)
+		@mkdir -p $(@D)
+		$(CC) -g -c -o $@ $< $(CFLAGS)
+
+
+$(LIB_DIR)/%.a: $(OBJ_STACK_ARRAY)
+		@mkdir -p $(LIB_DIR)
+		ar -rc $@ $^
+
+CFLAGS := $(CFLAGS) -L$(LIB_DIR) -l$(LIB_NAME) 
