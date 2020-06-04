198d6d08b2b2aa346ddb80011e7d3cbd002022dd
Suraj Pulloor <thegamer600@gmail.com>
2020-06-05 01:18:07 +0530

    Added ODIR_SLL, because we don't need to share it with circular_single_linked_list.

--- src/sll/single_linked_list.mk
+++ src/sll/single_linked_list.mk
@@ -9,6 +9,9 @@ ODIR:=$(ROOT_DIR)/$(ODIR)
 BIN_DIR:=$(ROOT_DIR)/$(BIN_DIR)
 LIB_DIR:=$(ROOT_DIR)/$(LIB_DIR)
 
+ODIR_SLL=sll
+ODIR:=$(ODIR)/$(ODIR_SLL)
+
 # Include files recipe
 _DEPS_SLL = single_linked_list.h
 DEPS_SLL = $(patsubst %,$(IDIR)/%,$(_DEPS_SLL))
@@ -17,13 +20,13 @@ DEPS_SLL = $(patsubst %,$(IDIR)/%,$(_DEPS_SLL))
 _OBJ_SLL = push.o pop.o misc.o get_set.o
 _OBJ_MAIN_SLL = ll.o
 
-OBJ_SLL = $(patsubst %,$(ODIR)/$(ODIR_SLL)/%,$(_OBJ_SLL))
-OBJ_MAIN_SLL = $(patsubst %,$(ODIR)/$(ODIR_SLL)/%,$(_OBJ_MAIN_SLL))
+OBJ_SLL = $(patsubst %,$(ODIR)/%,$(_OBJ_SLL))
+OBJ_MAIN_SLL = $(patsubst %,$(ODIR)/%,$(_OBJ_MAIN_SLL))
 
 _LIB_NAME = lib$(LIB_NAME).a
 GEN_LIB_LINKED_LIST = $(patsubst %,$(LIB_DIR)/%,$(_LIB_NAME))
 
-$(ODIR)/$(ODIR_SLL)/%.o: $(ROOT_DIR)/$(SRC)/$(SRC_SINGLE_LINKED_LIST)/%.c $(DEPS_SLL)
+$(ODIR)/%.o: %.c $(DEPS_SLL)
 		@mkdir -p $(@D)
 		$(CC) -g -c -o $@ $< $(CFLAGS)
 
