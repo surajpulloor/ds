6a1b9e0f49bfdb6429755f0fd5d3936a3e42e958
Suraj Pulloor <thegamer600@gmail.com>
2020-06-05 00:50:36 +0530

    Segregated build recipes related to stack linked list to its own Makefiles

---
+++ src/stack_ll/stack_linked_list.mk
@@ -0,0 +1,40 @@
+ROOT_DIR=../..
+SINGLE_LINKED_LIST_DIR=../sll
+
+include $(ROOT_DIR)/Makefile.variable
+
+LIB_NAME = stack_linked_list
+
+IDIR:=$(ROOT_DIR)/$(IDIR)
+ODIR:=$(ROOT_DIR)/$(ODIR)
+BIN_DIR:=$(ROOT_DIR)/$(BIN_DIR)
+LIB_DIR:=$(ROOT_DIR)/$(LIB_DIR)
+
+
+ODIR_STACK_LINKED_LIST=stack_ll
+
+
+_DEPS_STACK_LINKED_LIST = stack_ll.h
+DEPS_STACK_LINKED_LIST = $(patsubst %,$(IDIR)/%,$(_DEPS_STACK_LINKED_LIST))
+
+# Stack_Array objects recipes
+_OBJ_STACK_LINKED_LIST = push_pop.o misc.o
+OBJ_STACK_LINKED_LIST = $(patsubst %,$(ODIR)/$(ODIR_STACK_LINKED_LIST)/%,$(_OBJ_STACK_LINKED_LIST))
+
+_OBJ_MAIN_STACK_LINKED_LIST = main.o
+OBJ_MAIN_STACK_LINKED_LIST = $(patsubst %,$(ODIR)/$(ODIR_STACK_LINKED_LIST)/%,$(_OBJ_MAIN_STACK_LINKED_LIST))
+
+_LIB_NAME = lib$(LIB_NAME).a
+GEN_LIB_STACK_LINKED_LIST = $(patsubst %,$(LIB_DIR)/%,$(_LIB_NAME))
+
+
+$(ODIR)/$(ODIR_STACK_LINKED_LIST)/%.o: %.c $(DEPS_STACK_LINKED_LIST)
+		@mkdir -p $(@D)
+		$(CC) -g -c -o $@ $< $(CFLAGS)
+
+$(LIB_DIR)/%.a: $(OBJ_STACK_LINKED_LIST)
+		@mkdir -p $(LIB_DIR)
+		ar -rc $@ $^
+
+CFLAGS := $(CFLAGS) -L$(LIB_DIR) -l$(LIB_NAME) 
+
---
+++ src/stack_ll/stack_linked_list_main.mk
@@ -0,0 +1,14 @@
+include stack_linked_list.mk
+
+SECOND_LIB_NAME = sll
+
+CFLAGS := $(CFLAGS) -l$(SECOND_LIB_NAME)
+
+# build recipe for Stack_Array
+stack_linked_list_test: $(OBJ_MAIN_STACK_LINKED_LIST) $(GEN_LIB_STACK_LINKED_LIST)
+		cd $(SINGLE_LINKED_LIST_DIR); make -f single_linked_list_main.mk gen_lib
+		@mkdir -p $(BIN_DIR)
+		$(CC) -g -o $(BIN_DIR)/$@ $< $(CFLAGS)
+
+gen_lib: $(GEN_LIB_STACK_LINKED_LIST)
+		cd $(SINGLE_LINKED_LIST_DIR); make -f single_linked_list_main.mk gen_lib
