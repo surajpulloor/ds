900753563e91992f5c22d487641f1a40644f4c4f
Suraj Pulloor <thegamer600@gmail.com>
2020-06-06 17:40:31 +0530

    Changed the test recipe name to csll_test

--- src/csll/circular_single_linked_list_main.mk
+++ src/csll/circular_single_linked_list_main.mk
@@ -1,7 +1,7 @@
 include circular_single_linked_list.mk
 
 # build recipe for Single Linked List
-circular_single_linked_list_test: $(OBJ_MAIN_CIRCULAR_SINGLE_LINKED_LIST) $(GEN_LIB_CIRCULAR_SINGLE_LINKED_LIST)
+csll_test: $(OBJ_MAIN_CIRCULAR_SINGLE_LINKED_LIST) $(GEN_LIB_CIRCULAR_SINGLE_LINKED_LIST)
 		@mkdir -p $(BIN_DIR)
 		$(CC) -g -o $(BIN_DIR)/$@ $< $(CFLAGS)
 
