b0c745f4171f9e95848120f64b84f90200ce0c9d
Suraj Pulloor <thegamer600@gmail.com>
2020-06-07 00:20:47 +0530

    Changed stack_linked_list_test to stack_ll_test recipe

--- src/stack_ll/stack_linked_list_main.mk
+++ src/stack_ll/stack_linked_list_main.mk
@@ -5,7 +5,7 @@ SECOND_LIB_NAME = sll
 CFLAGS := $(CFLAGS) -l$(SECOND_LIB_NAME)
 
 # build recipe for Stack_Array
-stack_linked_list_test: $(OBJ_MAIN_STACK_LINKED_LIST) $(GEN_LIB_STACK_LINKED_LIST)
+stack_ll_test: $(OBJ_MAIN_STACK_LINKED_LIST) $(GEN_LIB_STACK_LINKED_LIST)
 		cd $(SINGLE_LINKED_LIST_DIR); make -f single_linked_list_main.mk gen_lib
 		@mkdir -p $(BIN_DIR)
 		$(CC) -g -o $(BIN_DIR)/$@ $< $(CFLAGS)
