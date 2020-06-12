9c25cb26651434650f4dc88d315776d1d3eaa827
Suraj Pulloor <thegamer600@gmail.com>
2020-06-12 20:23:52 +0530

    Corrected the function signature for copy_value() funcs

--- src/sll/copy_to_buffer.c
+++ src/sll/copy_to_buffer.c
@@ -1,35 +1,35 @@
 #include "../../include/single_linked_list.h"
 #include <string.h>
 
-void copy_value_to_buffer_int(SingleLinkedListNode* node, void* buffer)
+void copy_value_to_buffer_int(void* buffer, SingleLinkedListNode* node)
 {
     int* temp = (int*) buffer;
 
     *temp = *(int*) node->buffer;
 }
 
-void copy_value_to_buffer_float(SingleLinkedListNode* node, void* buffer)
+void copy_value_to_buffer_float(void* buffer, SingleLinkedListNode* node)
 {
     float* temp = (float*) buffer;
 
     *temp = *(float*) node->buffer;
 }
 
-void copy_value_to_buffer_double(SingleLinkedListNode* node, void* buffer)
+void copy_value_to_buffer_double(void* buffer, SingleLinkedListNode* node)
 {
     double* temp = (double*) buffer;
 
     *temp = *(double*) node->buffer;
 }
 
-void copy_value_to_buffer_char(SingleLinkedListNode* node, void* buffer)
+void copy_value_to_buffer_char(void* buffer, SingleLinkedListNode* node)
 {
     char* temp = (char*) buffer;
 
     *temp = *(char*) node->buffer;
 }
 
-void copy_value_to_buffer_char_ptr(SingleLinkedListNode* node, void* buffer)
+void copy_value_to_buffer_char_ptr(void* buffer, SingleLinkedListNode* node)
 {
     char* temp = (char*) buffer;
 
