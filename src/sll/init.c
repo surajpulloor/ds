bd0e05e14b2f3bac3796021994884db57a64c7df
Suraj Pulloor <thegamer600@gmail.com>
2020-06-12 20:31:31 +0530

    init_list() takes size_t as argument. this is done so that we are not limited to using linked list for primitive types. now linked list can used with any type possible.
    Made appropriate changes in init_list() funcs for primitive types to reflect the above changes

--- src/sll/init.c
+++ src/sll/init.c
@@ -1,73 +1,68 @@
 #include "../../include/single_linked_list.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-SingleLinkedList* init_list()
+SingleLinkedList* init_list(size_t data_type_size)
 {
     SingleLinkedList* list = (SingleLinkedList*) malloc(sizeof(SingleLinkedList));
     list->front = NULL;
     list->rear = NULL;
 
     list->size = 0;
+    list->data_type_size = data_type_size;
+
+    list->alloc_buffer = alloc_buffer;
+    list->free_buffer = free_buffer;
 
     return list;
 }
 
 SingleLinkedList* init_list_int()
 {
-    SingleLinkedList* list = init_list();
+    SingleLinkedList* list = init_list(sizeof(int));
     
-    list->alloc_buffer = alloc_buffer_int;
     list->copy_value_to_node = copy_value_to_node_int;
     list->copy_value_to_buffer = copy_value_to_buffer_int;
-    list->free_buffer = free_buffer;
 
     return list;
 }
 
 SingleLinkedList* init_list_float()
 {
-    SingleLinkedList* list = init_list();
+    SingleLinkedList* list = init_list(sizeof(float));
     
-    list->alloc_buffer = alloc_buffer_float;
     list->copy_value_to_node = copy_value_to_node_float;
     list->copy_value_to_buffer = copy_value_to_buffer_float;
-    list->free_buffer = free_buffer;
 
     return list;
 }
 
 SingleLinkedList* init_list_double()
 {
-    SingleLinkedList* list = init_list();
+    SingleLinkedList* list = init_list(sizeof(double));
     
-    list->alloc_buffer = alloc_buffer_double;
     list->copy_value_to_node = copy_value_to_node_double;
     list->copy_value_to_buffer = copy_value_to_buffer_double;
-    list->free_buffer = free_buffer;
 
     return list;
 }
 
 SingleLinkedList* init_list_char()
 {
-    SingleLinkedList* list = init_list();
+    SingleLinkedList* list = init_list(sizeof(char));
     
-    list->alloc_buffer = alloc_buffer_char;
     list->copy_value_to_node = copy_value_to_node_char;
     list->copy_value_to_buffer = copy_value_to_buffer_char;
-    list->free_buffer = free_buffer;
 
     return list;
 }
 
 SingleLinkedList* init_list_char_ptr()
 {
-    SingleLinkedList* list = init_list();
+    SingleLinkedList* list = init_list(sizeof(char) * MAX_BUFFER);
     
-    list->alloc_buffer = alloc_buffer_char_ptr;
     list->copy_value_to_node = copy_value_to_node_char_ptr;
     list->copy_value_to_buffer = copy_value_to_buffer_char_ptr;
-    list->free_buffer = free_buffer;
 
     return list;
 }
