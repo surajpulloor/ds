f9f0281e78686c9f9bf7d0e1edf33a3b263780df
Suraj Pulloor <thegamer600@gmail.com>
2020-06-14 01:58:09 +0530

    Changed the wrong pointer syntax

--- src/sll/setup_buffer_length/int.c
+++ src/sll/setup_buffer_length/int.c
@@ -4,48 +4,48 @@
 void setup_buffer_length_short(SingleLinkedListNode* node, void* value)
 {
     char buffer[MAX_CHARS_BUFFER];
-    node->buffer_length = sprintf(buffer, "%hi", *(*short) value);
+    node->buffer_length = sprintf(buffer, "%hi", *(short*) value);
 }
 
 void setup_buffer_length_unsigned_short(SingleLinkedListNode* node, void* value)
 {
     char buffer[MAX_CHARS_BUFFER];
-    node->buffer_length = sprintf(buffer, "%hu", *(*unsigned short) value);
+    node->buffer_length = sprintf(buffer, "%hu", *(unsigned short*) value);
 }
 
 void setup_buffer_length_int(SingleLinkedListNode* node, void* value)
 {
     char buffer[MAX_CHARS_BUFFER];
-    node->buffer_length = sprintf(buffer, "%d", *(*int) value);
+    node->buffer_length = sprintf(buffer, "%d", *(int*) value);
 }
 
 void setup_buffer_length_unsigned_int(SingleLinkedListNode* node, void* value)
 {
     char buffer[MAX_CHARS_BUFFER];
-    node->buffer_length = sprintf(buffer, "%lu", *(*unsigned int) value);
+    node->buffer_length = sprintf(buffer, "%u", *(unsigned int*) value);
 }
 
 void setup_buffer_length_long(SingleLinkedListNode* node, void* value)
 {
     char buffer[MAX_CHARS_BUFFER];
-    node->buffer_length = sprintf(buffer, "%ld", *(*long) value);
+    node->buffer_length = sprintf(buffer, "%ld", *(long*) value);
 }
 
 void setup_buffer_length_unsigned_long(SingleLinkedListNode* node, void* value)
 {
     char buffer[MAX_CHARS_BUFFER];
-    node->buffer_length = sprintf(buffer, "%lu", *(*unsigned long) value);
+    node->buffer_length = sprintf(buffer, "%lu", *(unsigned long*) value);
 }
 
 void setup_buffer_length_long_long(SingleLinkedListNode* node, void* value)
 {
     char buffer[MAX_CHARS_BUFFER];
-    node->buffer_length = sprintf(buffer, "%lld", *(*long long) value);
+    node->buffer_length = sprintf(buffer, "%lld", *(long long*) value);
 }
 
 void setup_buffer_length_unsigned_long_long(SingleLinkedListNode* node, void* value)
 {
     char buffer[MAX_CHARS_BUFFER];
-    node->buffer_length = sprintf(buffer, "%llu", *(*unsigned long long) value);
+    node->buffer_length = sprintf(buffer, "%llu", *(unsigned long long*) value);
 }
 
