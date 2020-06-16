bb0ac35dd4102b749b225556776a14d2eaed9a05
Suraj Pulloor <thegamer600@gmail.com>
2020-06-16 15:07:18 +0530

    Renamed misc.c to print.c. Removed non-print functions.

--- src/csll/print.c
+++ src/csll/print.c
@@ -3,42 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-CircularSingleLinkedList* init_list()
-{
-    CircularSingleLinkedList* list = (CircularSingleLinkedList*) malloc(sizeof(CircularSingleLinkedList));
-    list->front = NULL;
-    list->rear = NULL;
-
-    list->size = 0;
-
-    return list;
-}
-
-void free_list(CircularSingleLinkedList** list)
-{
-    if (*list == NULL) {
-        printf("error: list doesn't exist. please create one.\n");
-        return;
-    }
-
-    if ((*list)->front == NULL) {
-        printf("error: the list is empty. insert something.\n");
-        free(*list);
-        *list = NULL;
-        return;
-    }
-
-    CircularSingleLinkedListNode* node = (*list)->front;
-    
-    do {
-        CircularSingleLinkedListNode* nxtNode = node->next;
-        free(node);
-        node = nxtNode;
-    } while (node != (*list)->front);
-
-    free(*list);
-    *list = NULL;
-}
 
 void print_list(CircularSingleLinkedList* list)
 {
@@ -47,7 +11,7 @@ void print_list(CircularSingleLinkedList* list)
             CircularSingleLinkedListNode* node = list->front;
 
             do{
-                print_node(node, node == list->front, node == list->rear);
+                print_node(list, node, node == list->front, node == list->rear);
                 node = node->next;
             } while (node != list->front);
 
@@ -58,9 +22,9 @@ void print_list(CircularSingleLinkedList* list)
         printf("error: list doesn't exist. nothing to display.\n");
 }
 
-void print_node(CircularSingleLinkedListNode* node, bool isFront, bool isLast)
+void print_node(CircularSingleLinkedList* list, CircularSingleLinkedListNode* node, bool isFront, bool isLast)
 {
-    int len = strlen(node->data);
+    int len = node->buffer_length;
     int totalDashes = 19 + len;
 
     int halfway = (totalDashes + 6) / 2;
@@ -90,7 +54,10 @@ void print_node(CircularSingleLinkedListNode* node, bool isFront, bool isLast)
     }
 
     printf("\n||  | ");
-    printf("%p | %s", node->next, node->data);
+    printf("%p | ", node->next);
+
+    list->print_node_value(node);
+
     printf(" |\n");
 
     
