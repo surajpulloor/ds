e6c0c75cf8f0667c20d6eabdf29071af626a4580
Suraj Pulloor <thegamer600@gmail.com>
2020-06-16 15:09:50 +0530

    Added alloc and free functions to alloc_free.c

---
+++ src/csll/alloc_free.c
@@ -0,0 +1,39 @@
+#include "circular_single_linked_list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void* alloc_buffer(CircularSingleLinkedList* self, CircularSingleLinkedListNode* node)
+{
+    node->buffer = malloc(self->type_size);
+}
+
+void free_buffer(CircularSingleLinkedListNode* node)
+{
+    free(node->buffer);
+}
+
+
+void free_list(CircularSingleLinkedList** list)
+{
+    if (*list == NULL) {
+        printf("error: list doesn't exist. please create one.\n");
+        return;
+    }
+
+    if ((*list)->front == NULL) {
+        printf("error: the list is empty. insert something.\n");
+        free(*list);
+        *list = NULL;
+        return;
+    }
+    
+    for (CircularSingleLinkedListNode* node = (*list)->front; node; ) {
+        CircularSingleLinkedListNode* nxtNode = node->next;
+        free_buffer(node);
+        free(node);
+        node = nxtNode;
+    }
+
+    free(*list);
+    *list = NULL;
+}
