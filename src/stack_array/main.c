e3e732decdbc7bbeab7b603e13649286e6bda093
Suraj Pulloor <thegamer600@gmail.com>
2020-06-18 19:58:35 +0530

    Changed include path to absolute path. init_stack() call changed to init_stack_char_ptr().

--- src/stack_array/main.c
+++ src/stack_array/main.c
@@ -1,9 +1,9 @@
-#include "../../include/stack_array.h"
+#include "stack_array.h"
 #include <stdio.h>
 
 int main()
 {
-    Stack_Array* stack = init_stack();
+    Stack_Array* stack = init_stack_char_ptr();
 
     push(stack, "Oh mu God");
     push(stack, "kick");
@@ -13,13 +13,13 @@ int main()
 
     print_stack(stack, stack->top);
 
-    char buffer[MAX_CHARS];
-    printf("Value = %s\n", pop(stack, buffer));
-    printf("Value = %s\n", pop(stack, buffer));
-    printf("Value = %s\n", pop(stack, buffer));
-    printf("Value = %s\n", pop(stack, buffer));
-    printf("Value = %s\n", pop(stack, buffer));
-    printf("Value = %s\n", pop(stack, buffer));
+    char buffer[MAX_CHARS_BUFFER];
+    printf("Value = %s\n", (char*) pop(stack, buffer));
+    printf("Value = %s\n", (char*) pop(stack, buffer));
+    printf("Value = %s\n", (char*) pop(stack, buffer));
+    printf("Value = %s\n", (char*) pop(stack, buffer));
+    printf("Value = %s\n", (char*) pop(stack, buffer));
+    printf("Value = %s\n", (char*) pop(stack, buffer));
 
     print_stack(stack, stack->top);
 
