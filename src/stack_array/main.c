da3cd262945d13f1500bf8aa0f1608259cd1cc14
Suraj Pulloor <thegamer600@gmail.com>
2020-06-18 20:06:36 +0530

    Chnaged test code to int.

--- src/stack_array/main.c
+++ src/stack_array/main.c
@@ -3,23 +3,26 @@
 
 int main()
 {
-    Stack_Array* stack = init_stack_char_ptr();
+    Stack_Array* stack = init_stack_int();
 
-    push(stack, "Oh mu God");
-    push(stack, "kick");
-    push(stack, "me in teh");
-    push(stack, "cv");
-    push(stack, "lax");
+    // push(stack, "Oh mu God");
+    // push(stack, "kick");
+    // push(stack, "me in teh");
+    // push(stack, "cv");
+    // push(stack, "lax");
+
+    for (int i = 1; i < 100; i++)
+        push(stack, &i);
 
     print_stack(stack, stack->top);
 
     char buffer[MAX_CHARS_BUFFER];
-    printf("Value = %s\n", (char*) pop(stack, buffer));
-    printf("Value = %s\n", (char*) pop(stack, buffer));
-    printf("Value = %s\n", (char*) pop(stack, buffer));
-    printf("Value = %s\n", (char*) pop(stack, buffer));
-    printf("Value = %s\n", (char*) pop(stack, buffer));
-    printf("Value = %s\n", (char*) pop(stack, buffer));
+    printf("Value = %d\n", *(int*) pop(stack, buffer));
+    printf("Value = %d\n", *(int*) pop(stack, buffer));
+    printf("Value = %d\n", *(int*) pop(stack, buffer));
+    printf("Value = %d\n", *(int*) pop(stack, buffer));
+    printf("Value = %d\n", *(int*) pop(stack, buffer));
+    printf("Value = %d\n", *(int*) pop(stack, buffer));
 
     print_stack(stack, stack->top);
 
