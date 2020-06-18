b9151146e970ba8bc8296f899154466be6767604
Suraj Pulloor <thegamer600@gmail.com>
2020-06-18 19:56:34 +0530

    Added build recipes to reflect new c source files.

--- src/stack_array/stack_array_main.mk
+++ src/stack_array/stack_array_main.mk
@@ -3,6 +3,6 @@ include stack_array.mk
 # build recipe for Stack_Array
 stack_array_test: $(OBJ_MAIN_STACK_ARRAY) $(GEN_LIB_STACK_ARRAY)
 		@mkdir -p $(BIN_DIR)
-		$(CC) -g -o $(BIN_DIR)/$@ $^ $(CFLAGS)
+		$(CC) -g -o $(BIN_DIR)/$@ $< $(CFLAGS)
 
 gen_lib: $(GEN_LIB_STACK_ARRAY)
