bcf371b9e655303c83044790c16b0dca539989d5
Suraj Pulloor <thegamer600@gmail.com>
2020-06-20 16:50:40 +0530

    Changed the testing code a bit.

--- src/stack_ll/main.c
+++ src/stack_ll/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    Stack_Linked_List* stack = init_stack();
+    Stack_Linked_List* stack = init_stack_char_ptr();
 
     push(stack, "Oh mu God");
     push(stack, "kick");
@@ -14,12 +14,12 @@ int main()
     print_stack(stack);
 
     char buffer[MAX_CHARS];
-    printf("Value = %s\n", pop(stack, buffer));
-    printf("Value = %s\n", pop(stack, buffer));
-    printf("Value = %s\n", pop(stack, buffer));
-    printf("Value = %s\n", pop(stack, buffer));
-    printf("Value = %s\n", pop(stack, buffer));
-    printf("Value = %s\n", pop(stack, buffer));
+    printf("Value = %s\n", (char*) pop(stack, buffer));
+    printf("Value = %s\n", (char*) pop(stack, buffer));
+    printf("Value = %s\n", (char*) pop(stack, buffer));
+    printf("Value = %s\n", (char*) pop(stack, buffer));
+    printf("Value = %s\n", (char*) pop(stack, buffer));
+    printf("Value = %s\n", (char*) pop(stack, buffer));
 
     print_stack(stack);
 
