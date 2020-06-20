1ead17dd8296d3d22db29116f3e1278cd6aee012
Suraj Pulloor <thegamer600@gmail.com>
2020-06-20 16:47:43 +0530

    Added declarations for init_stack() for all types.

--- include/stack_ll.h
+++ include/stack_ll.h
@@ -4,6 +4,7 @@
 #define MAX_CHARS 1000
 
 #include "single_linked_list.h"
+#include <stdio.h>
 
 typedef struct {
     SingleLinkedList* list;
@@ -12,7 +13,36 @@ typedef struct {
 } Stack_Linked_List;
 
 
-Stack_Linked_List* init_stack();
+Stack_Linked_List* init_stack(size_t);
+
+// Integer init function
+Stack_Linked_List* init_stack_short();
+Stack_Linked_List* init_stack_unsigned_short();
+Stack_Linked_List* init_stack_int();
+Stack_Linked_List* init_stack_unsigned_int();
+Stack_Linked_List* init_stack_long();
+Stack_Linked_List* init_stack_unsigned_long();
+Stack_Linked_List* init_stack_long_long();
+Stack_Linked_List* init_stack_unsigned_long_long();
+
+
+// Float init function
+Stack_Linked_List* init_stack_float();
+Stack_Linked_List* init_stack_double();
+Stack_Linked_List* init_stack_long_double();
+
+
+// Char init function
+Stack_Linked_List* init_stack_char();
+Stack_Linked_List* init_stack_unsigned_char();
+
+
+// Pointer init function
+Stack_Linked_List* init_stack_char_ptr();
+
+// **** END: INIT FUNCTIONS *** //
+
+
 void push(Stack_Linked_List*, void*);
 void* pop(Stack_Linked_List*, void*);
 void print_stack(Stack_Linked_List*);
