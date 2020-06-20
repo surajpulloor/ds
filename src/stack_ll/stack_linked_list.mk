4105d1440a9862a4f25d9736faa43c37752ddb1c
Suraj Pulloor <thegamer600@gmail.com>
2020-06-20 16:50:28 +0530

    Added build recipe for building init_stack objects.

--- src/stack_ll/stack_linked_list.mk
+++ src/stack_ll/stack_linked_list.mk
@@ -8,6 +8,8 @@ LIB_NAME = stack_ll
 ODIR_STACK_LINKED_LIST=$(LIB_NAME)
 ODIR:=$(ODIR)/$(ODIR_STACK_LINKED_LIST)
 
+ODIR_INIT=$(ODIR)/$(SRC_INIT)
+
 
 _DEPS_STACK_LINKED_LIST = stack_ll.h
 DEPS_STACK_LINKED_LIST = $(patsubst %,$(IDIR)/%,$(_DEPS_STACK_LINKED_LIST))
@@ -15,6 +17,12 @@ DEPS_STACK_LINKED_LIST = $(patsubst %,$(IDIR)/%,$(_DEPS_STACK_LINKED_LIST))
 # Stack_Array objects recipes
 _OBJ_STACK_LINKED_LIST = push_pop.o misc.o
 OBJ_STACK_LINKED_LIST = $(patsubst %,$(ODIR)/%,$(_OBJ_STACK_LINKED_LIST))
+
+_OBJ_STACK_LINKED_LIST_TYPE_FUNCTIONS = int.o float.o char.o pointer.o
+_OBJ_STACK_LINKED_LIST_INIT_MAIN = init.o
+_OBJ_STACK_LINKED_LIST_INIT_MAIN := $(_OBJ_STACK_LINKED_LIST_TYPE_FUNCTIONS) $(_OBJ_STACK_LINKED_LIST_INIT_MAIN)
+OBJ_STACK_LINKED_LIST_INIT = $(patsubst %,$(ODIR_INIT)/%,$(_OBJ_STACK_LINKED_LIST_INIT_MAIN))
+
 
 _OBJ_MAIN_STACK_LINKED_LIST = main.o
 OBJ_MAIN_STACK_LINKED_LIST = $(patsubst %,$(ODIR)/%,$(_OBJ_MAIN_STACK_LINKED_LIST))
@@ -27,7 +35,12 @@ $(ODIR)/%.o: %.c $(DEPS_STACK_LINKED_LIST)
 		@mkdir -p $(@D)
 		$(CC) -g -c -o $@ $< $(CFLAGS)
 
-$(LIB_DIR)/%.a: $(OBJ_STACK_LINKED_LIST)
+$(ODIR_INIT)/%.o: $(SRC_INIT)/%.c $(DEPS_STACK_LINKED_LIST)
+		@mkdir -p $(@D)
+		$(CC) -g -c -o $@ $< $(CFLAGS)
+
+
+$(LIB_DIR)/%.a: $(OBJ_STACK_LINKED_LIST) $(OBJ_STACK_LINKED_LIST_INIT)
 		@mkdir -p $(LIB_DIR)
 		ar -rc $@ $^
 
