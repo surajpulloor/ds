8041d28e8598f91bc8cc1becac1be374c2d6de23
Suraj Pulloor <thegamer600@gmail.com>
2020-06-21 15:56:58 +0530

    Added a few test lines.

--- src/queue_array/queue_array_test.c
+++ src/queue_array/queue_array_test.c
@@ -1,29 +1,32 @@
-#include "../../include/queue_array.h"
+#include "queue_array.h"
 
 int main()
 {
-    Queue_Array* queue = init_queue();
+    Queue_Array* queue = init_queue_float();
 
-    enqueue(queue, "Hello, World");
-    enqueue(queue, "Damn it");
-    enqueue(queue, "Chills Gar");
-    enqueue(queue, "Like this");
-    enqueue(queue, "Houston");
+    // enqueue(queue, "Hello, World");
+    // enqueue(queue, "Damn it");
+    // enqueue(queue, "Chills Gar");
+    // enqueue(queue, "Like this");
+    // enqueue(queue, "Houston");
 
-    print_queue(queue);
-
-    char buffer[MAX_CHARS_BUFFER];
-
-    dequeue(queue, buffer);
-    dequeue(queue, buffer);
-    dequeue(queue, buffer);
-    dequeue(queue, buffer);
-    dequeue(queue, buffer);
-
-    enqueue(queue, "New Stuff");
-    enqueue(queue, "Newer Stuff");
+    for (float i = 1.623; i < 500; i += 2.58)
+        enqueue(queue, &i);
 
     print_queue(queue);
+
+    // char buffer[MAX_CHARS_BUFFER];
+
+    // dequeue(queue, buffer);
+    // dequeue(queue, buffer);
+    // dequeue(queue, buffer);
+    // dequeue(queue, buffer);
+    // dequeue(queue, buffer);
+
+    // enqueue(queue, "New Stuff");
+    // enqueue(queue, "Newer Stuff");
+
+    // print_queue(queue);
 
     free_queue(&queue);
 
