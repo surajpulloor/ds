c35053220225b0fbaf80432c6d33c20b86de0339
Suraj Pulloor <thegamer600@gmail.com>
2020-06-21 15:27:09 +0530

    Implemented setup_print_node_value_func() and setup_largest_string_length_func().

--- src/stack_array/setup_funcs.c
+++ src/stack_array/setup_funcs.c
@@ -9,3 +9,19 @@ void setup_copy_value_funcs(
     list->copy_value_to_node = copy_value_to_node;
     list->copy_value_to_buffer = copy_value_to_buffer;
 }
+
+void setup_print_node_value_func(
+	Stack_Array* stack, 
+	int (*print_node_value_func)(Stack_Array*, int)
+)
+{
+	stack->print_node_value = print_node_value_func;
+}
+
+void setup_largest_string_length_func(
+	Stack_Array* stack, 
+	void (*largest_string_length_func)(Stack_Array*, void*)
+)
+{
+	stack->setup_largest_string_length = largest_string_length_func;
+}
