57a50be757df9a1f07dd1199e5738efc2449b6ea
Suraj Pulloor <thegamer600@gmail.com>
2020-06-21 17:39:17 +0530

    Added declarations for init_queue() for all types. Changed value type from char* to void*

--- include/queue_ll.h
+++ include/queue_ll.h
@@ -6,12 +6,40 @@
 typedef struct {
     SingleLinkedList* list;
     int size;
-} Queue_Linked_list;
+} Queue_Linked_List;
 
-Queue_Linked_list* init_queue();
-void enqueue(Queue_Linked_list*, char*);
-char* dequeue(Queue_Linked_list*, char*);
-void print_queue(Queue_Linked_list*);
-void free_queue(Queue_Linked_list**);
+Queue_Linked_List* init_queue(size_t);
+
+// Integer init function
+Queue_Linked_List* init_queue_short();
+Queue_Linked_List* init_queue_unsigned_short();
+Queue_Linked_List* init_queue_int();
+Queue_Linked_List* init_queue_unsigned_int();
+Queue_Linked_List* init_queue_long();
+Queue_Linked_List* init_queue_unsigned_long();
+Queue_Linked_List* init_queue_long_long();
+Queue_Linked_List* init_queue_unsigned_long_long();
+
+
+// Float init function
+Queue_Linked_List* init_queue_float();
+Queue_Linked_List* init_queue_double();
+Queue_Linked_List* init_queue_long_double();
+
+
+// Char init function
+Queue_Linked_List* init_queue_char();
+Queue_Linked_List* init_queue_unsigned_char();
+
+
+// Pointer init function
+Queue_Linked_List* init_queue_char_ptr();
+
+// **** END: INIT FUNCTIONS *** //
+
+void enqueue(Queue_Linked_List*, void*);
+void* dequeue(Queue_Linked_List*, void*);
+void print_queue(Queue_Linked_List*);
+void free_queue(Queue_Linked_List**);
 
 #endif
