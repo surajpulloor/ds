4d8f2a1dc90c9c520045d1ca844f171cd5cba071
Suraj Pulloor <thegamer600@gmail.com>
2020-06-23 00:33:17 +0530

    Implemented copy() funcs.

--- src/string/copy.c
+++ src/string/copy.c
@@ -1,18 +1,49 @@
-#include "ds/string.h"
+#include "ds/buffered_string.h"
 #include <string.h>
+#include <stdlib.h>
 
 String* copy_char_str(String* string, char* value)
 {
-    if ()
+    if (strlen(value) > string->capacity) {
+        string->buffer = (char*) realloc(string->buffer, string->capacity * 2);
+        string->capacity *= 2;
+    }
+
+    string->length = strlen(value);
+    strcpy(string->buffer, value);
+
+    return string;
 }
 
-String* copy_str(String* str1, String* str2)
+String* copy_str(String* dest, String* src)
 {
+    if (dest->capacity < src->length) {
+        dest->buffer = (char*) realloc(dest->buffer, dest->capacity * 2);
+        dest->capacity *= 2;
+    }
 
+    dest->length = src->length;
+    strcpy(dest->buffer, src->buffer);
+
+    return dest;
 }
 
 
 char* copy_to_buffer(char* buffer, unsigned int buffer_size, String* string)
 {
+    if (buffer_size < string->length) {
+        printf("error: char buffer is small. please pass in a large buffer\n\t   you need to add space for %d chars\n", string->length - buffer_size);
+        return NULL;
+    }
 
+    strcpy(buffer, string->buffer);
+    
+}
+
+char* copy_substring(char* dest, char* src, unsigned int begin, unsigned int end)
+{
+    for (int i = 0; begin < end; begin++, i++)
+        dest[i] = src[begin];
+
+    return dest;
 }
