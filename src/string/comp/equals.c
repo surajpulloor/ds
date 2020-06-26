a0c5af992a83ee7bbaa4159c68b3461522f8a992
Suraj Pulloor <thegamer600@gmail.com>
2020-06-26 12:48:15 +0530

    Added equals comparison ops.

--- src/string/comp/equals.c
+++ src/string/comp/equals.c
@@ -1,7 +1,23 @@
 #include "ds/buffered_string.h"
+#include <string.h>
 
-bool equals_char_str(String*, char*);
-bool equals_str(String*, String*);
+bool equals_char_str(String* str, char* c_str)
+{
+    return strcmp(str->buffer, c_str) == 0 ? true : false;
+}
 
-bool not_equals_char_str(String*, char*);
-bool not_equals_str(String*, String*);
+bool equals_str(String* str1, String* str2)
+{
+    return strcmp(str1->buffer, str2->buffer) == 0 ? true : false;
+}
+
+
+bool not_equals_char_str(String* str, char* c_str)
+{
+    return !equals_char_str(str, c_str);
+}
+
+bool not_equals_str(String* str1, String* str2)
+{
+    return !equals_str(str1, str2);
+}
