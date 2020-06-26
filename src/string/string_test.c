43fcc6f2c00418ae1a022821d9b452421770bf69
Suraj Pulloor <thegamer600@gmail.com>
2020-06-26 21:24:07 +0530

    Implemented substring() and substring_to_buffer().

--- src/string/string_test.c
+++ src/string/string_test.c
@@ -2,10 +2,13 @@
 
 int main()
 {
+    printf("Init...\n");
     String* str = init_str();
     String* str2 = init_str();
 
-    copy_char_str(str, "Hello, World is great");
+
+    printf("Copy...\n");
+    copy_char_str(str, "Hello, World");
     
     copy_str(str2, str);
 
@@ -16,6 +19,62 @@ int main()
     printf("str buffer: %s\n", str->buffer);
     printf("str2 buffer: %s\n", str2->buffer);
     printf("char buffer: %s\n", buffer);
+
+    printf("\nEquals & Not Equals...\n");
+    
+    if (not_equals_char_str(str, "Hello, World"))
+        printf("str != c_str\n");
+
+    copy_char_str(str2, "Hello, W");
+
+    if (not_equals_str(str, str2))
+        printf("str1 != str2\n");
+
+    printf("\nLess & Less Than\n");
+
+    String* str3 = init_str();
+
+    copy_char_str(str3, "Alabama");
+
+    if (less_than_char_str(str3, "Peter Dickney"))
+        printf("str < c_str\n");
+
+    if (less_than_equal_char_str(str3, "Alabama"))
+        printf("str <= c_str\n");
+
+    printf("\nGreater & Greater Than\n");
+
+    String* str4 = init_str();
+
+    copy_char_str(str4, "Peter Dickney");
+
+    if (greater_than_char_str(str4, "Zootic Animal"))
+        printf("str > c_str\n");
+
+    if (greater_than_equal_char_str(str4, "Peter Dickney"))
+        printf("str >= c_str\n");
+
+    printf("\nSubstring ops\n");
+
+    String* str5 = substring(str4, 3, 100);
+
+    if (str5) {
+        printf("%s\n", str5->buffer);
+        free_string(str5);
+    }
+
+    char buffer2[5];
+
+    substring_to_buffer(buffer2, 5, str4, 3, 9);
+
+    printf("%s\n", buffer2);
+
+
+    free_string(str);
+    free_string(str2);
+    free_string(str3);
+    free_string(str4);
+    
 
     
     return 0;
--- src/string/substr.c
+++ src/string/substr.c
@@ -1,4 +1,52 @@
 #include "ds/buffered_string.h"
+#include <string.h>
 
-char* substring(String*, int, int);
-char* substring_to_buffer(char*, unsigned int, String*, int, int);
+String* substring(String* str, int begin, int end)
+{
+    if (begin < 0 || begin > str->length) {
+        printf("error: the begin bound is off limits.\n please provide a value within string bounds.\n");
+        return NULL;
+    }
+
+    if (end < 0 || end > str->length) {
+        printf("error: the end bound is off limits.\n please provide a value within string bounds.\n");
+        return NULL;
+    }
+
+    if (begin >= end) {
+        printf("error: the begin bound cannot be greater than end bound.\n");
+        return NULL;
+    }
+
+    String* new_str = init_string(end - begin);
+
+    copy_substring(new_str->buffer, str->buffer, begin, end);
+
+    return new_str;
+}
+
+char* substring_to_buffer(char* buffer, unsigned int buffer_size, String* str, int begin, int end)
+{
+    if (begin < 0 || begin > str->length) {
+        printf("error: the begin bound is off limits.\n please provide a value within string bounds.\n");
+        return NULL;
+    }
+
+    if (end < 0 || end > str->length) {
+        printf("error: the end bound is off limits.\n please provide a value within string bounds.\n");
+        return NULL;
+    }
+
+    if (begin >= end) {
+        printf("error: the begin bound cannot be greater than end bound.\n");
+        return NULL;
+    }
+
+    if (buffer_size < end - begin) {
+        printf("error: the buffer cannot hold the substring.\n");
+        return NULL;
+    }
+
+    return copy_substring(buffer, str->buffer, begin, end);
+}
+
