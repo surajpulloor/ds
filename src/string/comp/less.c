bc2ab7bf4aa72e4b34146613d71ebe671e57e6fb
Suraj Pulloor <thegamer600@gmail.com>
2020-06-26 13:16:29 +0530

    Implemented less and less_than_equal funcs.

--- src/string/comp/less.c
+++ src/string/comp/less.c
@@ -1,7 +1,23 @@
 #include "ds/buffered_string.h"
+#include <string.h>
 
-bool less_than_char_str(String*, char*);
-bool less_than_str(String*, String*);
+bool less_than_char_str(String* str, char* c_str)
+{
+    return strcmp(str->buffer, c_str) < 0;
+}
 
-bool less_than_equal_char_str(String*, char*);
-bool less_than_equal_str(String*, String*);
+bool less_than_str(String* str1, String* str2)
+{
+    return strcmp(str1->buffer, str2->buffer) < 0;
+}
+
+
+bool less_than_equal_char_str(String* str, char* c_str)
+{
+    return less_than_char_str(str, c_str) || equals_char_str(str, c_str);
+}
+
+bool less_than_equal_str(String* str1, String* str2)
+{
+    return less_than_str(str1, str2) || equals_str(str1, str2);
+}
