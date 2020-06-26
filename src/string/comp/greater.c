5367316793a3b2b111bdd21ea06dcf6e89af1899
Suraj Pulloor <thegamer600@gmail.com>
2020-06-26 13:28:34 +0530

    Implemented greater_than and greater_than_equal fro c_str and str types.

--- src/string/comp/greater.c
+++ src/string/comp/greater.c
@@ -1,7 +1,22 @@
 #include "ds/buffered_string.h"
+#include <string.h>
 
-bool greater_than_char_str(String*, char*);
-bool greater_than_str(String*, String*);
+bool greater_than_char_str(String* str, char* c_str)
+{
+    return strcmp(str->buffer, c_str) > 0;
+}
 
-bool greater_than_equal_str(String*, String*);
-bool greater_than_equal_str(String*, String*);
+bool greater_than_str(String* str1, String* str2)
+{
+    return strcmp(str1->buffer, str1->buffer) > 0;
+}
+
+bool greater_than_equal_char_str(String* str, char* c_str)
+{
+    return greater_than_char_str(str, c_str) || equals_char_str(str, c_str);
+}
+
+bool greater_than_equal_str(String* str1, String* str2)
+{
+    return greater_than_str(str1, str2) || equals_str(str1, str2);
+}
