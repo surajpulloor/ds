269e326fb947bd4b06adfe2b32e9ac74a1cfdbe3
Suraj Pulloor <thegamer600@gmail.com>
2020-06-26 12:50:18 +0530

    Removed ternary op, not needed. The result of the equality is enough.

--- src/string/comp/equals.c
+++ src/string/comp/equals.c
@@ -3,12 +3,12 @@
 
 bool equals_char_str(String* str, char* c_str)
 {
-    return strcmp(str->buffer, c_str) == 0 ? true : false;
+    return strcmp(str->buffer, c_str) == 0;
 }
 
 bool equals_str(String* str1, String* str2)
 {
-    return strcmp(str1->buffer, str2->buffer) == 0 ? true : false;
+    return strcmp(str1->buffer, str2->buffer) == 0;
 }
 
 
