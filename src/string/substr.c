8b2515254ff242ffa2d4a375c13344921755408f
Suraj Pulloor <thegamer600@gmail.com>
2020-06-29 21:54:45 +0530

    Re-wrote substring_to_buffer() to allow substring to the end of the string. The previous implementation was exclusive to the end of the string in reverse substring.

--- src/string/substr.c
+++ src/string/substr.c
@@ -19,46 +19,43 @@ String* substring(String* str, int begin, int end)
 
 char* substring_to_buffer(char* buffer, unsigned int buffer_size, String* str, int begin, int end)
 {
+
+    // copy empty string by default
+    strcpy(buffer, "");
+
     int new_begin;
     int new_end;
 
     if (begin < 0) {
 
-        if (begin < str->length * -1) {
-            printf("error: begin is beyound the string length.\n");
-            return NULL;
-        }
-
-        new_begin = begin + str->length;
+        if (begin < str->length * -1) 
+            new_begin = -1;
+        else 
+            new_begin = begin + str->length;
 
     } else {
         
-        if (begin > str->length) {
-            printf("error: begin is beyound the string length.\n");
-            return NULL;
-        }
+        if (begin > str->length) 
+            new_begin = str->length;
+        else 
+            new_begin = begin;
 
-        new_begin = begin;
     }
 
     
     if (end < 0) {
 
-        if (end < str->length * -1) {
-            printf("error: end is beyound the string length.\n");
-            return NULL;
-        }
-
-        new_end = end + str->length;
+        if (end < str->length * -1) 
+            new_end = -1;
+        else 
+            new_end = end + str->length;
 
     } else {
         
-        if (end > str->length) {
-            printf("error: end is beyound the string length.\n");
-            return NULL;
-        }
-
-        new_end = end;
+        if (end > str->length) 
+            new_end = str->length;
+        else 
+            new_end = end;
     }
 
 
@@ -68,11 +65,11 @@ char* substring_to_buffer(char* buffer, unsigned int buffer_size, String* str, i
     }
 
 
-    int substring_length = abs(new_end - new_begin + 1);
+    int substring_length = abs(new_end - new_begin) + 1;
 
     if (buffer_size < substring_length) {
         printf("error: the buffer is small please provide a bigger buffer. need space for %d more chars.\n", substring_length - buffer_size);
-        return NULL;
+        return buffer;
     }
 
 
