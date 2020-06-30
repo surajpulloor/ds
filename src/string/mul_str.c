b49d1311052b4d21ab2c2b063a1dbdf450a736e8
Suraj Pulloor <thegamer600@gmail.com>
2020-06-30 23:04:18 +0530

    Implemented all the mul_str funcs.

--- src/string/mul_str.c
+++ src/string/mul_str.c
@@ -1,4 +1,30 @@
 #include "ds/buffered_string.h"
+#include <string.h>
 
-String* mul_str(String*, unsigned int);
-String* mul_str_to_buffer(char*, unsigned int, String*, unsigned int);
+String* mul_str(String* str, unsigned int no)
+{
+    String* mul_string = init_string(str->length * no + 1);
+
+    copy_char_str(mul_string, "");
+
+    for (int i = 0; i < no; i++)
+        strcat(mul_string->buffer, str->buffer);
+
+    return mul_string;
+}
+
+char* mul_str_to_buffer(char* buffer, unsigned int buffer_size, String* str, unsigned int no)
+{
+    if (buffer_size < str->length * no + 1) {
+        int needed_buffer = (str->length * no + 1) - buffer_size;
+        printf("error: the buffer isn't big enough, please create a bigger buffer. need %d more chars.\n", needed_buffer);
+        return buffer;
+    }
+
+    strcpy(buffer, "");
+
+    for (int i = 0; i < no; i++)
+        strcat(buffer, str->buffer);
+
+    return buffer;
+}
