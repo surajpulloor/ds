bc8106660b37401024a7a28daeebf80019d4225e
Suraj Pulloor <thegamer600@gmail.com>
2020-07-28 21:15:56 +0530

    Added append_lists func declarations.

---
+++ include/ds/sll/ops/append.h
@@ -0,0 +1,11 @@
+#ifndef _APPEND_H
+#define _APPEND_H
+
+#include "../types.h"
+
+SingleLinkedList* append_lists(SingleLinkedList*, SingleLinkedList*);
+SingleLinkedList* append_lists_copy(SingleLinkedList*, SingleLinkedList*);
+SingleLinkedList* append_lists_copies(SingleLinkedList*, SingleLinkedList*);
+
+
+#endif
