20da5a38e8fe9ae2ca0c4f2b91751717b079a653
Suraj Pulloor <thegamer600@gmail.com>
2020-08-03 01:10:26 +0530

    Added declarations for greater & greater_equals funcs for all primitive's.

--- include/ds/sll/comp/greater.h
+++ include/ds/sll/comp/greater.h
@@ -9,10 +9,50 @@ bool greater_than_nodes(SingleLinkedList*, SingleLinkedListNode*, SingleLinkedLi
 bool greater_than_equal_indices(SingleLinkedList*, unsigned int, unsigned int);
 bool greater_than_equal_nodes(SingleLinkedList*, SingleLinkedListNode*, SingleLinkedListNode*);
 
-bool greater_than_number(SingleLinkedListNode*, SingleLinkedListNode*);
+
+// greater_than
+bool greater_than_short(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_unsigned_short(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_int(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_unsigned_int(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_long(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_unsigned_long(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_long_long(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_unsigned_long_long(SingleLinkedListNode*, SingleLinkedListNode*);
+
+
+bool greater_than_float(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_double(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_long_double(SingleLinkedListNode*, SingleLinkedListNode*);
+
+
+bool greater_than_char(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_unsigned_char(SingleLinkedListNode*, SingleLinkedListNode*);
+
 bool greater_than_char_ptr(SingleLinkedListNode*, SingleLinkedListNode*);
 
-bool greater_than_equal_number(SingleLinkedListNode*, SingleLinkedListNode*);
+
+// greater_than_equal
+bool greater_than_equal_short(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_equal_unsigned_short(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_equal_int(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_equal_unsigned_int(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_equal_long(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_equal_unsigned_long(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_equal_long_long(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_equal_unsigned_long_long(SingleLinkedListNode*, SingleLinkedListNode*);
+
+
+bool greater_than_equal_float(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_equal_double(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_equal_long_double(SingleLinkedListNode*, SingleLinkedListNode*);
+
+
+bool greater_than_equal_char(SingleLinkedListNode*, SingleLinkedListNode*);
+bool greater_than_equal_unsigned_char(SingleLinkedListNode*, SingleLinkedListNode*);
+
 bool greater_than_equal_char_ptr(SingleLinkedListNode*, SingleLinkedListNode*);
+
+
 
 #endif
