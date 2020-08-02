ef4bc038412b56decc6dee3a2cf612d4796177c2
Suraj Pulloor <thegamer600@gmail.com>
2020-08-03 01:10:39 +0530

    Added declarations for less & less_equals funcs for all primitive's.

--- include/ds/sll/comp/less.h
+++ include/ds/sll/comp/less.h
@@ -9,10 +9,48 @@ bool less_than_nodes(SingleLinkedList*, SingleLinkedListNode*, SingleLinkedListN
 bool less_than_equal_indices(SingleLinkedList*, unsigned int, unsigned int);
 bool less_than_equal_nodes(SingleLinkedList*, SingleLinkedListNode*, SingleLinkedListNode*);
 
-bool less_than_number(SingleLinkedListNode*, SingleLinkedListNode*);
+// less_than
+bool less_than_short(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_unsigned_short(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_int(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_unsigned_int(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_long(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_unsigned_long(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_long_long(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_unsigned_long_long(SingleLinkedListNode*, SingleLinkedListNode*);
+
+
+bool less_than_float(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_double(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_long_double(SingleLinkedListNode*, SingleLinkedListNode*);
+
+
+bool less_than_char(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_unsigned_char(SingleLinkedListNode*, SingleLinkedListNode*);
+
 bool less_than_char_ptr(SingleLinkedListNode*, SingleLinkedListNode*);
 
-bool less_than_equal_number(SingleLinkedListNode*, SingleLinkedListNode*);
+
+// less_than_equal
+bool less_than_equal_short(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_equal_unsigned_short(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_equal_int(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_equal_unsigned_int(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_equal_long(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_equal_unsigned_long(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_equal_long_long(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_equal_unsigned_long_long(SingleLinkedListNode*, SingleLinkedListNode*);
+
+
+bool less_than_equal_float(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_equal_double(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_equal_long_double(SingleLinkedListNode*, SingleLinkedListNode*);
+
+
+bool less_than_equal_char(SingleLinkedListNode*, SingleLinkedListNode*);
+bool less_than_equal_unsigned_char(SingleLinkedListNode*, SingleLinkedListNode*);
+
 bool less_than_equal_char_ptr(SingleLinkedListNode*, SingleLinkedListNode*);
+
 
 #endif
