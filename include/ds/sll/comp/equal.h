e33566eb104f29aa39024727cd2ab014d3294e1e
Suraj Pulloor <thegamer600@gmail.com>
2020-08-03 01:09:58 +0530

    Added declarations for equal funcs for all primitive's.

--- include/ds/sll/comp/equal.h
+++ include/ds/sll/comp/equal.h
@@ -6,7 +6,23 @@
 bool equal_indices(SingleLinkedList*, unsigned int, unsigned int);
 bool equal_nodes(SingleLinkedList*, SingleLinkedListNode*, SingleLinkedListNode*);
 
-bool equal_number(SingleLinkedListNode*, SingleLinkedListNode*);
+bool equal_short(SingleLinkedListNode*, SingleLinkedListNode*);
+bool equal_unsigned_short(SingleLinkedListNode*, SingleLinkedListNode*);
+bool equal_int(SingleLinkedListNode*, SingleLinkedListNode*);
+bool equal_unsigned_int(SingleLinkedListNode*, SingleLinkedListNode*);
+bool equal_long(SingleLinkedListNode*, SingleLinkedListNode*);
+bool equal_unsigned_long(SingleLinkedListNode*, SingleLinkedListNode*);
+bool equal_long_long(SingleLinkedListNode*, SingleLinkedListNode*);
+bool equal_unsigned_long_long(SingleLinkedListNode*, SingleLinkedListNode*);
+
+
+bool equal_float(SingleLinkedListNode*, SingleLinkedListNode*);
+bool equal_double(SingleLinkedListNode*, SingleLinkedListNode*);
+bool equal_long_double(SingleLinkedListNode*, SingleLinkedListNode*);
+
+bool equal_char(SingleLinkedListNode*, SingleLinkedListNode*);
+bool equal_unsigned_char(SingleLinkedListNode*, SingleLinkedListNode*);
+
 bool equal_char_ptr(SingleLinkedListNode*, SingleLinkedListNode*);
 
 #endif
