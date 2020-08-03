4965c723e284533b76249aabc993dc2a462dbd9b
Suraj Pulloor <thegamer600@gmail.com>
2020-08-04 01:18:48 +0530

    Implemented all greater_than_equal() funcs.

---
+++ src/sll/comp/greater_equal/int.c
@@ -0,0 +1,41 @@
+#include "ds/sll/single_linked_list.h"
+
+bool greater_than_equal_short(SingleLinkedListNode* node1, SingleLinkedListNode* node2)
+{
+    COMP(short, >=)
+}
+
+bool greater_than_equal_unsigned_short(SingleLinkedListNode* node1, SingleLinkedListNode* node2)
+{
+    COMP(unsigned short, >=)
+}
+
+bool greater_than_equal_int(SingleLinkedListNode* node1, SingleLinkedListNode* node2)
+{
+    COMP(int, >=)
+}
+
+bool greater_than_equal_unsigned_int(SingleLinkedListNode* node1, SingleLinkedListNode* node2)
+{
+    COMP(unsigned int, >=)
+}
+
+bool greater_than_equal_long(SingleLinkedListNode* node1, SingleLinkedListNode* node2)
+{
+    COMP(long, >=)
+}
+
+bool greater_than_equal_unsigned_long(SingleLinkedListNode* node1, SingleLinkedListNode* node2)
+{
+    COMP(unsigned long, >=)
+}
+
+bool greater_than_equal_long_long(SingleLinkedListNode* node1, SingleLinkedListNode* node2)
+{
+    COMP(long long, >=)
+}
+
+bool greater_than_equal_unsigned_long_long(SingleLinkedListNode* node1, SingleLinkedListNode* node2)
+{
+    COMP(unsigned long long, >=)
+}
