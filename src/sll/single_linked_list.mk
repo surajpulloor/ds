9a930d3712dfc6e072a934fac80c3eb4529c315b
Suraj Pulloor <thegamer600@gmail.com>
2020-08-05 00:52:10 +0530

    Included the newly created mk files in the recipes dir

--- src/sll/single_linked_list.mk
+++ src/sll/single_linked_list.mk
@@ -4,7 +4,11 @@ include $(ROOT_DIR)/Makefile.variable
 
 include $(MAKEFILE_DIR)/constants.mk
 include $(MAKEFILE_DIR)/inter_recipes.mk
-include $(MAKEFILE_DIR)/recipes.mk
+
+include $(MAKEFILE_DIR)/recipes/lib.mk
+include $(MAKEFILE_DIR)/recipes/comp.mk
+include $(MAKEFILE_DIR)/recipes/copy.mk
+include $(MAKEFILE_DIR)/recipes/misc.mk
 
 # build recipe for Single Linked List
 sll_test: $(OBJ_MAIN_SINGLE_LINKED_LIST) $(GEN_LIB_LINKED_LIST)
